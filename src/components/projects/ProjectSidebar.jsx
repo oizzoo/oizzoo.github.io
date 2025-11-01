fb0c023512608bcc8e7861c6090d07f10658195c
oizzoo <108180322+oizzoo@users.noreply.github.com>
2025-11-01 14:39:02 +0100

    feat(projects): add interactive category sidebar with 'All' filter

--- src/components/projects/ProjectSidebar.jsx
+++ src/components/projects/ProjectSidebar.jsx
@@ -1,4 +1,31 @@
+import "./ProjectSidebar.css"
+import projectsData from "../../data/Projects";
+import { useState, useMemo } from "react";
+
+
 function ProjectSidebar() {
-  return <aside>Sidebar (Categories + projects)</aside>;
+    const categories = useMemo(
+    () => ["All", ...Object.keys(projectsData)],
+    [projectsData]
+  );
+
+  const [activeCategory, setActiveCategory] = useState("All");
+
+    return (
+    <aside>
+      <div className="categories">
+        {categories.map((category) => (
+          <button
+            key={category}
+            className={`category-btn ${activeCategory === category ? "active" : ""}`}
+            onClick={() => setActiveCategory(category)}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+    </aside>
+  );
+     
 }
 export default ProjectSidebar;
