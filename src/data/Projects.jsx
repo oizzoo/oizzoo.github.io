0b2db39edd580bc1fcb307a8091245692e552a15
oizzoo <108180322+oizzoo@users.noreply.github.com>
2025-12-01 14:31:13 +0100

    fix: add https to the demo link in TypeScript project so it redirects correctly

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -106,7 +106,7 @@ const projects = {
       desc: "First mini-app to learn TS with REACT",
       tech: ["React", "Vite", "TypeScript"],
       github: "https://github.com/oizzoo/first-TS-mini-project",
-      demo: "oizzoo.github.io/first-TS-mini-project/",
+      demo: "https://oizzoo.github.io/first-TS-mini-project/",
       video: "./videos/first-typescript-app.mp4"
     }
   ]
